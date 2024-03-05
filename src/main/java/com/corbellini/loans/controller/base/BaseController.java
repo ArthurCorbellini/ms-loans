76dd61458aede107884f181f30d292b11729f583
Arthur Corbellini <arthur.corbellini27@gmail.com>
2024-03-04 21:35:33 -0300

    private to protected BaseController

--- src/main/java/com/corbellini/loans/controller/base/BaseController.java
+++ src/main/java/com/corbellini/loans/controller/base/BaseController.java
@@ -2,20 +2,20 @@ package com.corbellini.loans.controller.base;
 
 public class BaseController {
 
-  public static final String STATUS_200 = "200";
-  public static final String MESSAGE_200 = "Request processed successfully";
+  protected static final String STATUS_200 = "200";
+  protected static final String MESSAGE_200 = "Request processed successfully";
 
-  public static final String STATUS_201 = "201";
-  public static final String MESSAGE_201 = "Register created successfully";
+  protected static final String STATUS_201 = "201";
+  protected static final String MESSAGE_201 = "Register created successfully";
 
-  public static final String STATUS_417 = "417";
-  public static final String MESSAGE_417_UPDATE =
+  protected static final String STATUS_417 = "417";
+  protected static final String MESSAGE_417_UPDATE =
       "Update operation failed. Please try again or contact Dev team";
-  public static final String MESSAGE_417_DELETE =
+  protected static final String MESSAGE_417_DELETE =
       "Delete operation failed. Please try again or contact Dev team";
 
-  public static final String STATUS_500 = "500";
-  public static final String MESSAGE_500 =
+  protected static final String STATUS_500 = "500";
+  protected static final String MESSAGE_500 =
       "An error occurred. Please try again or contact Dev team";
 
 }
